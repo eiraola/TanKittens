c47b81503b97853afb4ec8e4fe7fd830814ac321
Eduardo Montero <emontero.iraola@gmail.com>
2024-05-04 15:33:56 +0200

    Added an enemy that shoots the player

--- Assets/_CaTanks/Scripts/Weapons/ProjectileLauncher.cs
+++ Assets/_CaTanks/Scripts/Weapons/ProjectileLauncher.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class ProjectileLauncher : MonoBehaviour
 {
     [SerializeField] private Projectile _projectileGO;
+    [SerializeField] private float _fireCadence = 1.0f;
+    private float _lastTimeShoot = 0.0f;
     public Transform firePoint;
 
     public void SpawnProjectile()
     {
-        if (!_projectileGO) { return; } 
+        if (!_projectileGO) { return; }
+        if (_lastTimeShoot + _fireCadence > Time.time) { return; }
+
+        _lastTimeShoot = Time.time;
         Instantiate(_projectileGO, firePoint.position, firePoint.rotation);
     
     }
